4d82267f50e7db981ca374fbc6227dec3317872d
Andy Wong <awong@cogolabs.com>
2015-05-11 15:35:26 -0400

    Added handlebars support, basic views

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
-var express = require('express');
-var app = express();
+// Server Listner
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+// Import things:
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+
+// Express app:
+var app = express();
+app.engine('handlebars', exphbs({defaultLayout: 'base'}));
+app.set('view engine', 'handlebars');
+
+
+// Routes:
+app.get('/', function(req, res) {
+  res.render('home', {
+        helpers: {
+            testText: function() { return 'This is a test!'; }
+        }
+    });;
 });
 
-var server = app.listen(3000, function () {
+app.get('/report/:text', function(req, res) {
+  res.send('Report: ' + req.params.text);
+});
 
+
+// Start server:
+var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-
+  console.log('Listening at http://%s:%s', host, port);
 });
