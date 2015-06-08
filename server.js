4c0d1c51943064cf28aca3e67ee2eaf8947db98e
Andy Wong <awong@cogolabs.com>
2015-06-08 10:52:25 -0400

    moved date prototype to other file

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ var nib = require('nib');
 var uuid = require('node-uuid');
 
 // CUSTOM LIBRARIES
+var now = require('./lib/date');
 var grabAccountInfo = require('./lib/auth');
 
 // EXPRESS APP
@@ -59,46 +60,6 @@ app.use(stormpath.init(app, {
 
 
 // PROTOTYPES
-
-// Converts single digits to double digits
-Date.prototype.addzero = function(num) {
-  return (num >= 0 && num < 10) ? "0" + num : num + "";
-};
-
-// Converts 24hr format to 12hr format
-Date.prototype.standardHours = function() {
-  var hours = this.getHours();
-  if (hours > 12) {
-      hours -= 12;
-  } else if (hours === 0) {
-     hours = 12;
-  };
-  return hours;
-};
-
-// Creates a MM/DD/YYYY HH:MM:SS timestamp
-Date.prototype.timestamp = function() {
-  return [
-    [this.addzero(this.getMonth() + 1),
-    this.addzero(this.getDate()),
-    this.getFullYear()].join("/"),
-    [this.addzero(this.standardHours()), this.addzero(this.getMinutes()),
-    this.addzero(this.getSeconds())].join(":"),
-    now.getHours() >= 12 ? "PM" : "AM"
-  ].join(" ");
-};
-
-// Creates a raw YYYYMMDDHHMMSS timestamp
-Date.prototype.rawstamp = function() {
-  return [
-    [this.getFullYear(),
-    this.addzero(this.getMonth() + 1),
-    this.addzero(this.getDate())
-    ].join(""),
-    [this.addzero(this.getHours()), this.addzero(this.getMinutes()),
-    this.addzero(this.getSeconds())].join("")
-  ].join("");
-};
 
 // Checks if string is empty
 String.prototype.isEmpty = function() {
