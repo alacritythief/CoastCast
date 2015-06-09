3bbf108648c063085e00ec0824e676fb88dacdc1
Andy Wong <awong@cogolabs.com>
2015-06-09 11:08:11 -0400

    moved user count to header

--- public/js/app.js
+++ public/js/app.js
@@ -10,12 +10,6 @@ $(document).ready(function() {
 
   // Update Usercount
   socket.on('usercount', function(msg) {
-    $('.message-usercount').animate({
-      backgroundColor: "#666",
-    }, 1000);
-    $('.message-usercount').animate({
-      backgroundColor: "#000000",
-    }, 500);
     $('#usercount').text(msg);
   });
 
