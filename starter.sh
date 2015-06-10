9e0d4485e9ee3b9f16e84afa9a1ca53a160cae53
Andy Wong <alacritythief@gmail.com>
2015-06-10 11:59:00 -0400

    Added starting script

---
+++ starter.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
+then
+        export PATH=/usr/local/bin:$PATH
+        forever start --sourceDir /home/alacritythief/CoastCast server.js >> /home/alacritythief/CoastCast/server.log 2>&1
+fi
